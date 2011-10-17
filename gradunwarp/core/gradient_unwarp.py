a64791c57e1396c997a7b5692880da87e0559636
Krish Subramaniam <krish.subramaniam@gmail.com>
2011-10-17 18:23:33 -0400

    still at an unusable stage

--- gradunwarp/core/gradient_unwarp.py
+++ gradunwarp/core/gradient_unwarp.py
@@ -4,16 +4,35 @@
 #   copyright and license terms.
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-import optparse as opt
+import argparse as arg
 import logging
-import constants
+import globals
 
-log = constants.getlogger()
+log = globals.get_logger()
 
 
-def options_parse_gradunwarp():
-    # initiate the option parser
-    p = opt.OptionParser(version=constants.VERSION, usage=constants.usage)
+def argument_parse_gradunwarp():
+    '''Arguments parser from the command line
+    '''
+    # initiate
+    p = arg.ArgumentParser(version=globals.VERSION, usage=globals.usage)
+
+    # required arguments
+    p.add_argument('infile', action='store')
+    p.add_argument('outfile', action='store')
+    p.add_argument('vendor', action='store', choices=['siemens', 'ge'])
+
+    coef_grp = p.add_mutually_exclusive_group(required=True)
+    coef_grp.add_argument('-g', '--gradfile', dest='gradfile')
+    coef_grp.add_argument('-c', '--coeffile', dest='coeffile')
+
+    # optional arguments
+    p.add_argument('-w', '--warp', action='store_true', default=False)
+    p.add_argument('-n', '--nojacobian', dest='nojac', action='store_true',
+                  default=False)
+    p.add_argument('--verbose', action='store_true', default=False)
+
+    return p.parse_args()
 
 
 class GradientUnwarpRunner(object):
@@ -21,13 +40,15 @@ class GradientUnwarpRunner(object):
     run() method performs the actual unwarping
     write() method performs the writing of the unwarped volume
     '''
-    def __init__(self, options):
+    def __init__(self, args):
         ''' constructor takes the option datastructure which is the
         result of (options, args) = parser.parse_args()
         '''
-        self.opts = options
+        self.args = args
 
-        log.setLevel(logging.DEBUG if self.opts.verbose else logging.INFO)
+        log.setLevel(logging.INFO)
+        if hasattr(self.args, 'verbose'):
+            log.setLevel(logging.DEBUG)
 
     def run(self):
         pass
@@ -37,9 +58,9 @@ class GradientUnwarpRunner(object):
 
 
 if __name__ == '__main__':
-    opts = options_parse_gradunwarp()
+    args = argument_parse_gradunwarp()
 
-    grad_unwarp = GradientUnwarpRunner(opts)
+    grad_unwarp = GradientUnwarpRunner(args)
 
     grad_unwarp.run()
 
