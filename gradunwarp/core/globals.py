302b67f4059975469ea82f78f89dc94ce1dfac93
Krish Subramaniam <krish.subramaniam@gmail.com>
2011-10-17 18:24:57 -0400

    added file

---
+++ gradunwarp/core/globals.py
@@ -0,0 +1,22 @@
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the gradunwarp package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+import logging
+
+VERSION = '2.0.0'
+
+usage = 'foo'
+
+
+def get_logger():
+    log = logging.getLogger('gradunwarp')
+    log.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(name)s-%(levelname)s: %(message)s")
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+    return log
